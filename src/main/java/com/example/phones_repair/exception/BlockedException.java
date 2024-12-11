3f5df4fafeb5405445b76ae24ddf033924000786
temirbaevabermet <temirbaevabermet4@gmail.com>
2024-12-11 21:15:15 +0600

    added exceptions for code

---
+++ src/main/java/com/example/phones_repair/exception/BlockedException.java
@@ -0,0 +1,12 @@
+package com.example.phones_repair.exception;
+
+public class BlockedException extends RuntimeException {
+    public BlockedException() {
+        super();
+    }
+
+    public BlockedException(String message) {
+        super(message);
+    }
+
+}
